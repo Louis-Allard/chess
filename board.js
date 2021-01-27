4face6d64893fd1a10336d01960cd25ff29007be
Louis Allard <la.devweb@yahoo.com>
2021-01-27 16:39:59 +0100

    Add Square name on board.

--- board.js
+++ board.js
@@ -1,15 +1,15 @@
 let nombre = 1;
+let col = ["A", "B", "C", "D", "E", "F", "G", "H"];
 document.write('<table>');
 for (let l = 9; l > 0; l--) {
-    document.write('<tr>');
+    document.write('<tr><td class="pos">' + l + '</td>');
     for (let c = 1; c < 9; c++) {
         if (nombre / 2 == Math.round(nombre / 2)) { var color = "caseBrun" }
         else { var color = "caseBlanche" }
-        let col = ["A", "B", "C", "D", "E", "F", "G", "H"];
         for (let i = 0; i < col.length; i++) {
-            let j=i+1;
-            if(j === c ) {
-            document.write('<td><div id="' + l + '|' + c + '" class="' + color + '" title="' + col[i] + ' | ' + l + '"></div></td>');
+            let j = i + 1;
+            if (j === c) {
+                document.write('<td><div id="' + l + '|' + c + '" class="' + color + '" title="' + col[i] + ' | ' + l + '"></div></td>');
             }
         }
         nombre++;
@@ -17,4 +17,10 @@ for (let l = 9; l > 0; l--) {
     nombre++;
     document.write('</tr>');
 }
+document.write('<tr><td>&nbsp;</td>')
+for (let p = 0; p < col.length;p++) {
+    document.write('<td class="pos">' + col[p] + '</td>');
+}
+document.write('</tr>')
+
 document.write('</table>');
