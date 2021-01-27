8db0a69eb6ab879def11ade01296e9aba7afd32f
Louis Allard <la.devweb@yahoo.com>
2021-01-27 15:02:57 +0100

    Remove comment and fix var name.

--- board.js
+++ board.js
@@ -1,17 +1,17 @@
-var nombre=1;
+let nombre=1;
 document.write('<table>');
-for(var l=1; l<9; l++) // CREER 8 LIGNES
+for(let l=1; l<9; l++) 
 {
 document.write('<tr>');
-for(var c=1; c<9; c++) // CREER 8 CASES PAR LIGNE
+for(let c=1; c<9; c++) 
 {
     
-if (nombre/2 == Math.round(nombre/2)) // classe CHANGE UNE CASE SUR DEUX (SI NOMBRE EST PAIR OU PAS)
-{var classe="caseBrun"}
+if (nombre/2 == Math.round(nombre/2))
+{let classe="caseBrun"}
 else
-{var classe="caseBlanc"}
+{let classe="caseBlanc"}
     
-document.write('<td><a onMouseOver="position('+l+','+c+')"><div id="'+l+'|'+c+'" class="'+classe+'"></div></a></td>'); // CREATION DES CASES (position('+l+','+c+') EST COMPLETE PAR UNE FONCTION DE LOCALISATION DES CASES DANS UN AUTRE SCRIPT)
+document.write('<td><a onMouseOver="position('+l+','+c+')"><div id="'+l+'|'+c+'" class="'+classe+'"></div></a></td>');
 nombre++;
 }
 nombre++;
