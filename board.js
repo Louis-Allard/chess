400964698b8cfecbc86143634b2b0572c6c33920
Louis Allard <la.devweb@yahoo.com>
2021-01-27 15:23:31 +0100

    Add square position.

--- board.js
+++ board.js
@@ -1,18 +1,14 @@
-let nombre=1;
+var nombre = 1;
 document.write('<table>');
-for(let l=1; l<9; l++) 
-{
-document.write('<tr>');
-for(let c=1; c<9; c++) 
-{
-    
-if (nombre/2 == Math.round(nombre/2))
-{let classe="caseBrun"}
-else
-{let classe="caseBlanc"}
-    
-document.write('<td><a onMouseOver="position('+l+','+c+')"><div id="'+l+'|'+c+'" class="'+classe+'"></div></a></td>');
-nombre++;
+for (var l = 1; l < 9; l++) {
+    document.write('<tr>');
+for (var c = 1; c < 9; c++) {
+
+    if (nombre / 2 == Math.round(nombre / 2)) { var classe = "caseBrun" }
+    else { var classe = "caseBlanc" }
+
+    document.write('<td><a onMouseOver="position(' + l + ',' + c + ')"><div id="' + l + '|' + c + '" class="' + classe + '" title="' + c + ' | ' + l + '"></div></a></td>');
+    nombre++;
 }
 nombre++;
 document.write('</tr>');
