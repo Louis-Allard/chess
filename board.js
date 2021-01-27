094ce49f5efb8b2a0be011847843bd3295f5ac9f
Louis Allard <la.devweb@yahoo.com>
2021-01-27 18:08:53 +0100

    Add white pieces.

--- board.js
+++ board.js
@@ -10,7 +10,7 @@
 let nombre = 1;
 let col = ["A", "B", "C", "D", "E", "F", "G", "H"];
 document.write('<table>');
-for (let l = 9; l > 0; l--) {
+for (let l = 8; l > 0; l--) {
     document.write('<tr><td class="pos">' + l + '</td>');
     for (let c = 1; c < 9; c++) {
         if (nombre / 2 == Math.round(nombre / 2)) { var color = "caseBrun" }
@@ -18,8 +18,29 @@ for (let l = 9; l > 0; l--) {
         for (let i = 0; i < col.length; i++) {
             let j = i + 1;
             if (j === c) {
-                document.write('<td><div id="' + l + '|' + c + '" class="' + color + '" title="' + col[i] + ' | ' + l + '"></div></td>');
+                document.write('<td><div id="' + l + '|' + c + '" class="' + color + '" title="' + col[i] + ' | ' + l + '">');
+                if (col[i] === "E" && l === 1) {
+                    document.write('<div class="w_king"></div>')
+                }
+                if (col[i] === "D" && l === 1) {
+                    document.write('<div class="w_queen"></div>')
+                }
+                if ((col[i] === "C" && l === 1) || (col[i] === "F" && l === 1)) {
+                    document.write('<div class="b_queen"></div>')
+                }
+                if ((col[i] === "B" && l === 1) || (col[i] === "G" && l === 1)) {
+                    document.write('<div class="b_knight"></div>')
+                }
+                if ((col[i] === "A" && l === 1) || (col[i] === "H" && l === 1)) {
+                    document.write('<div class="b_tower"></div>')
+                }
+                if ((col[i] === "A" && l === 2) || (col[i] === "B" && l === 2) || (col[i] === "C" && l === 2) || (col[i] === "D" && l === 2) || (col[i] === "E" && l === 2) || (col[i] === "F" && l === 2) || (col[i] === "G" && l === 2) || (col[i] === "H" && l === 2)) {
+                    document.write('<div class="b_pawn"></div>')
+                }                                                                 
+                document.write('</div>');
+
             }
+
         }
         nombre++;
     }
@@ -27,7 +48,7 @@ for (let l = 9; l > 0; l--) {
     document.write('</tr>');
 }
 document.write('<tr><td>&nbsp;</td>')
-for (let p = 0; p < col.length;p++) {
+for (let p = 0; p < col.length; p++) {
     document.write('<td class="pos">' + col[p] + '</td>');
 }
 document.write('</tr>')
