d89c854d5bb17f4218369690a1ca031022b5a25f
Louis Allard <la.devweb@yahoo.com>
2021-02-02 17:14:51 +0100

    Update files.

--- chess.js
+++ chess.js
@@ -20,40 +20,40 @@ for (let l = 8; l > 0; l--) {
             if (j === c) {
                 document.write('<td><div id="' + l + '|' + c + '" class="' + color + '">');
                 if (col[i] === "E" && l === 1) {
-                    document.write('<div id="w_king" class="w_king on" title="King" onclick="moves(id)"></div>')
+                    document.write('<div id="w_king" class="w_king on" title="King" onclick="named(id)"></div>')
                 }
                 if (col[i] === "D" && l === 1) {
-                    document.write('<div id="w_queen" class="w_queen on" title="Queen" onclick="moves(id)"></div>')
+                    document.write('<div id="w_queen" class="w_queen on" title="Queen" onclick="named(id)"></div>')
                 }
                 if ((col[i] === "C" && l === 1) || (col[i] === "F" && l === 1)) {
-                    document.write('<div id="w_bishop" class="w_bishop on" title="Bishop" onclick="moves(id)"></div>')
+                    document.write('<div id="w_bishop" class="w_bishop on" title="Bishop" onclick="named(id)"></div>')
                 }
                 if ((col[i] === "B" && l === 1) || (col[i] === "G" && l === 1)) {
-                    document.write('<div id="w_knight" class="w_knight on" title="Knight" onclick="moves(id)"></div>')
+                    document.write('<div id="w_knight" class="w_knight on" title="Knight" onclick="named(id)"></div>')
                 }
                 if ((col[i] === "A" && l === 1) || (col[i] === "H" && l === 1)) {
-                    document.write('<div id="w_tower" class="w_tower on" title="Tower" onclick="moves(id)"></div>')
+                    document.write('<div id="w_tower" class="w_tower on" title="Tower" onclick="named(id)"></div>')
                 }
                 if ((col[i] === "A" && l === 2) || (col[i] === "B" && l === 2) || (col[i] === "C" && l === 2) || (col[i] === "D" && l === 2) || (col[i] === "E" && l === 2) || (col[i] === "F" && l === 2) || (col[i] === "G" && l === 2) || (col[i] === "H" && l === 2)) {
-                    document.write('<div id="w_pawn"class="w_pawn on" title="Pawn" onclick="moves(id)"></div>')
+                    document.write('<div id="w_pawn"class="w_pawn on" title="Pawn" onclick="named(id)"></div>')
                 }
                 if (col[i] === "E" && l === 8) {
-                    document.write('<div id="b_king" class="b_king on" title="King" onclick="moves(id)"></div>')
+                    document.write('<div id="b_king" class="b_king on" title="King" onclick="named(id)"></div>')
                 }
                 if (col[i] === "D" && l === 8) {
-                    document.write('<div id="b_queen" class="b_queen on" title="Queen" onclick="moves(id)"></div>')
+                    document.write('<div id="b_queen" class="b_queen on" title="Queen" onclick="named(id)"></div>')
                 }
                 if ((col[i] === "C" && l === 8) || (col[i] === "F" && l === 8)) {
-                    document.write('<div id="b_bishop" class="b_bishop on" title="Bishop" onclick="moves(id)"></div>')
+                    document.write('<div id="b_bishop" class="b_bishop on" title="Bishop" onclick="named(id)"></div>')
                 }
                 if ((col[i] === "B" && l === 8) || (col[i] === "G" && l === 8)) {
-                    document.write('<div id="b_knight" class="b_knight on" title="Knight" onclick="moves(id)"></div>')
+                    document.write('<div id="b_knight" class="b_knight on" title="Knight" onclick="named(id)"></div>')
                 }
                 if ((col[i] === "A" && l === 8) || (col[i] === "H" && l === 8)) {
-                    document.write('<div id="b_tower" class="b_tower on" title="Tower" onclick="moves(id)"></div>')
+                    document.write('<div id="b_tower" class="b_tower on" title="Tower" onclick="named(id)"></div>')
                 }
                 if ((col[i] === "A" && l === 7) || (col[i] === "B" && l === 7) || (col[i] === "C" && l === 7) || (col[i] === "D" && l === 7) || (col[i] === "E" && l === 7) || (col[i] === "F" && l === 7) || (col[i] === "G" && l === 7) || (col[i] === "H" && l === 7)) {
-                    document.write('<div id="b_pawn" class="b_pawn on" title="Pawn" onclick="moves(id)"></div>')
+                    document.write('<div id="b_pawn" class="b_pawn on" title="Pawn" onclick="named(id)"></div>')
                 }
                 document.write('</div>');
 
@@ -96,7 +96,7 @@ function pieces(n, pos_x, pos_y, disp) {
     }
 };
 
-function moves(id) {
+function named(id) {
     url = "http://127.0.0.1:5500/moves.json";
     fetch(url)
         .then((response) => {
@@ -108,11 +108,8 @@ function moves(id) {
         })
         .then((jsonResponse) => {
             console.log("------------------------");
-            console.log("jsonResponse : ", jsonResponse);
+            console.log("Piece proprety : ", jsonResponse[id]);
             console.log("------------------------");
-            for(let i=0; i<url.length;i++) {
-                console.log(url.color)
-            }
         }).catch((error) => {
             console.log(error);
         });
